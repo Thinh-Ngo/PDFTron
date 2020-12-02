6effd177ae71838c8bbca66f59be33f74e1dada8
Thinh Ngo <74173746+Thinh-Ngo@users.noreply.github.com>
2020-12-02 08:43:58 +0700

    Create modal.js

---
+++ modal.js
@@ -0,0 +1,34 @@
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    if (!event.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
+
+document.getElementById('cancel-methods').addEventListener('click', () => {
+    modal.style.display = "none"
+})
